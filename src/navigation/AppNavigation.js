396857b61c8e66a28b9b2b655722b4b340fc4686
Makrem Zitoun <makrem.zitoun@x-cite.io>
2020-10-31 22:31:34 +0100

    clean project

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -10,25 +10,11 @@ import SignUpScreen from '../screens/SignUp/SignUpScreen';
 import HomeScreen from '../screens/Home/HomeScreen';
 import PortfolioScreen from '../screens/Portfolio/PortfolioScreen';
 import ExpensesScreen from '../screens/Expenses/ExpensesScreen';
-import BankAccountsScreen from '../screens/BankAccounts/BankAccountsScreen';
-import MoreScreen from '../screens/More/MoreScreen';
-import AllSpendingCategoriesScreen from '../screens/AllSpendingCategories/AllSpendingCategoriesScreen';
-import NewsScreen from '../screens/News/NewsScreen';
-import NotificationsScreen from '../screens/Notifications/NotificationsScreen';
+
 import ProfileScreen from '../screens/Profile/ProfileScreen';
-import AccountDetailsScreen from '../screens/AccountDetails/AccountDetailsScreen';
-import SettingsScreen from '../screens/Settings/SettingsScreen';
-import LinkAccountsScreen from '../screens/LinkAccount/LinkAccountScreen';
-import BankAccountDetalisScreen from '../screens/BankAccountDetails/BankAccountDetailsScreen';
-import BuySellScreen from '../screens/BuySell/BuySellScreen';
-import SearchScreen from '../screens/Search/SearchScreen';
-import TransactionsScreen from '../screens/Transactions/TransactionsScreen';
-import NewsWebView from '../screens/NewsWebView/NewsWebView';
 import DrawerContainer from '../screens/DrawerContainer/DrawerContainer';
 import MenuImage from '../components/MenuImage/MenuImage';
-import AssetScreen from '../screens/AssetScreen/AssetScreen';
-import AllAssetsScreen from '../screens/AllAssetsScreen/AllAssetsScreen';
-import AssetsWatchListScreen from '../screens/AssetsWatchListScreen/AssetsWatchListScreen';
+
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createDrawerNavigator} from '@react-navigation/drawer';
@@ -41,28 +27,7 @@ const Stack = createStackNavigator();
 const BottomTabNavigator = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
-// login stack
-const Login = createStackNavigator();
-const LoginStack = () => {
-  return (
-    <Login.Navigator
-      initialRouteName="LogIn"
-      headerMode="float"
-      screenOptions={() => ({})}>
-      <Login.Screen
-        options={{headerShown: false}}
-        name="LogIn"
-        component={LogInScreen}
-      />
-      <Login.Screen name="SignUp" component={SignUpScreen} />
-      <Login.Screen
-        options={{headerShown: false}}
-        name="Welcome"
-        component={WelcomeScreen}
-      />
-    </Login.Navigator>
-  );
-};
+
 
 const LandingNavigator = () => {
   return (
@@ -198,14 +163,7 @@ const mainScreensNavigator = () => {
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Portfolio" component={PortfolioScreen} />
       <Stack.Screen name="Expenses" component={ExpensesScreen} />
-      <Stack.Screen name="More" component={MoreScreen} />
-      <Stack.Screen
-        options={{
-          title: 'Accounts',
-        }}
-        name="BankAccounts"
-        component={BankAccountsScreen}
-      />
+    
     </Stack.Navigator>
   );
 };
@@ -238,258 +196,8 @@ const MainNavigator = () => {
         component={TabNavigator}
         //component={Platform.OS === 'ios' ? TabNavigator : mainScreensNavigator}
       />
-      <Stack.Screen
-        options={({navigation}) => ({
-          headerLeft: () => (
-            <BackButton
-              onPress={() => navigation.goBack()}
-              title={'Expenses'}
-            />
-          ),
-          title: 'All Expenses',
-        })}
-        name="AllSpendingCategories"
-        component={AllSpendingCategoriesScreen}
-      />
-      <Stack.Screen
-        options={({navigation}) => ({
-          headerLeft: () =>
-            Platform.OS == 'ios' ? (
-              <BackButton onPress={() => navigation.goBack()} title={'More'} />
-            ) : (
-              <MenuImage
-                onPress={() => {
-                  navigation.openDrawer();
-                }}
-              />
-            ),
-          title: 'News',
-          headerRight: () => <View />,
-        })}
-        name="News"
-        component={NewsScreen}
-      />
-      <Stack.Screen
-        options={({navigation}) => ({
-          headerLeft: () =>
-            Platform.OS == 'ios' ? (
-              <BackButton onPress={() => navigation.goBack()} title={'More'} />
-            ) : (
-              <MenuImage
-                onPress={() => {
-                  navigation.openDrawer();
-                }}
-              />
-            ),
-          title: 'Notifications',
-          headerRight: () => <View />,
-        })}
-        name="Notifications"
-        component={NotificationsScreen}
-      />
-      <Stack.Screen
-        options={({navigation}) => ({
-          headerLeft: () =>
-            Platform.OS == 'ios' ? (
-              <BackButton onPress={() => navigation.goBack()} title={'More'} />
-            ) : (
-              <MenuImage
-                onPress={() => {
-                  navigation.openDrawer();
-                }}
-              />
-            ),
-          title: 'Profile',
-          headerRight: () => <View />,
-        })}
-        name="Profile"
-        component={ProfileScreen}
-      />
-      <Stack.Screen
-        options={({navigation}) => ({
-          headerLeft: () => (
-            <BackButton onPress={() => navigation.goBack()} title={'Profile'} />
-          ),
-          title: 'Account Details',
-          headerRight: () => <View />,
-        })}
-        name="AccountDetails"
-        component={AccountDetailsScreen}
-      />
-      <Stack.Screen
-        options={({navigation}) => ({
-          headerLeft: () => (
-            <BackButton onPress={() => navigation.goBack()} title={'Profile'} />
-          ),
-          title: 'Account Details',
-          headerRight: () => <View />,
-        })}
-        name="Settings"
-        component={SettingsScreen}
-      />
-      <Stack.Screen
-        options={({navigation}) => ({
-          title: 'Link New Account',
-          headerLeft: () => (
-            <BackButton onPress={() => navigation.goBack()} title={''} />
-          ),
-          headerRight: () => (
-            <TouchableHighlight
-              onPress={() => this.addAccount()}
-              underlayColor="rgba(73,182,77,1,0.9)">
-              <Text
-                style={{color: 'white', fontWeight: 'bold', marginRight: 10}}>
-                Done
-              </Text>
-            </TouchableHighlight>
-          ),
-        })}
-        name="LinkAccount"
-        component={LinkAccountsScreen}
-      />
-      <Stack.Screen
-        options={({navigation, route}) => ({
-          headerLeft: () => (
-            <BackButton onPress={() => navigation.goBack()} title={''} />
-          ),
-          title: route.params?.title,
-          headerRight: () => <View />,
-        })}
-        name="BankAccountDetalis"
-        component={BankAccountDetalisScreen}
-      />
-      <Stack.Screen
-        options={({navigation}) => ({
-          headerLeft: () => (
-            <TouchableHighlight
-              onPress={() => navigation.goBack()}
-              underlayColor="rgba(73,182,77,1,0.9)">
-              <Text style={{color: 'white', marginLeft: 10}}>Cancel</Text>
-            </TouchableHighlight>
-          ),
-          headerRight: () => <View />,
-          title: route.params?.type + ' ' + route.params?.title,
-        })}
-        name="BuySell"
-        component={BuySellScreen}
-      />
-      <Stack.Screen
-        options={({navigation}) => ({
-          headerLeft: () => (
-            <BackButton onPress={() => navigation.goBack()} title={''} />
-          ),
-          headerRight: () => <View />,
-          headerTitle: () => (
-            <SearchBar
-              containerStyle={{
-                backgroundColor: 'transparent',
-                borderBottomColor: 'transparent',
-                borderTopColor: 'transparent',
-                flex: 1,
-              }}
-              inputContainerStyle={{
-                backgroundColor: 'white',
-              }}
-              inputStyle={{
-                backgroundColor: 'white',
-                borderRadius: 10,
-                color: 'black',
-              }}
-              searchIcond
-              clearIcon
-              //lightTheme
-              round
-              onChangeText={(text) => this.handleSearch(text)}
-              //onClear={() => params.handleSearch('')}
-              placeholder="Search"
-              value={this.getValue}
-            />
-          ),
-        })}
-        name="Search"
-        component={SearchScreen}
-      />
-      <Stack.Screen
-        name="Transactions"
-        options={({navigation, route}) => ({
-          headerLeft: () => (
-            <BackButton
-              onPress={() => navigation.goBack()}
-              title={route.params.backScreen}
-            />
-          ),
-          title: 'Transactions',
-          headerRight: () => <View />,
-        })}
-        component={TransactionsScreen}
-      />
-      <Stack.Screen
-        options={({navigation}) => ({
-          headerStyle: {
-            backgroundColor: 'transparent',
-          },
-          title: '',
-        })}
-        name="NewsWebView"
-        component={NewsWebView}
-      />
-      <Stack.Screen name="AssetScreen" component={AssetScreen} />
-      <Stack.Screen
-        options={({navigation, route}) => ({
-          title: 'Your ' + route.params?.title,
-          headerLeft: () => (
-            <BackButton
-              onPress={() => navigation.goBack()}
-              title={'Portfolio'}
-            />
-          ),
-          headerRight: () => (
-            <View style={{flexDirection: 'row'}}>
-              <TouchableHighlight onPress={() => this.onPressBinocular()}>
-                <Image
-                  style={{
-                    width: 20,
-                    height: 20,
-                    marginRight: 10,
-                    alignSelf: 'center',
-                  }}
-                  source={require('../../assets/icons/binocular.png')}
-                />
-              </TouchableHighlight>
-              <TouchableHighlight
-                onPress={() =>
-                  navigation.navigate('Search', {type: route.params?.title})
-                }>
-                <Image
-                  source={require('../../assets/icons/search.png')}
-                  style={{
-                    marginRight: 10,
-                    width: 20,
-                    height: 20,
-                    alignSelf: 'center',
-                  }}
-                />
-              </TouchableHighlight>
-            </View>
-          ),
-        })}
-        name="AllAssetsScreen"
-        component={AllAssetsScreen}
-      />
-      <Stack.Screen
-        options={({navigation, route}) => ({
-          headerLeft: (
-            <BackButton
-              onPress={() => navigation.goBack()}
-              title={route.params?.type}
-            />
-          ),
-          title: 'Your WatchList',
-          headerRight: () => <View />,
-        })}
-        name="AssetsWatchListScreen"
-        component={AssetsWatchListScreen}
-      />
+     
+   
     </Stack.Navigator>
   );
 };
