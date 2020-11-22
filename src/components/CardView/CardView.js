9a78135923c6dc8013854772e5e108ee769f21cf
Makrem Zitoun <makrem.zitoun@x-cite.io>
2020-11-22 11:41:32 +0100

    page history

--- src/components/CardView/CardView.js
+++ src/components/CardView/CardView.js
@@ -2,38 +2,37 @@
 //portfolio card
 //card with image left,double text middle, and procent right
 import React from 'react';
-import { View, Image, Text, TouchableHighlight } from 'react-native';
+import {View, Image, Text, TouchableHighlight} from 'react-native';
 import styles from './styles';
 
 class CardView extends React.Component {
   render() {
     const item = this.props.item;
-    return (
+    return item.isTitle ? (
+      <TouchableHighlight underlayColor="rgba(73,182,77,1,0.9)">
+        <View style={styles.itemTitleContainer}>
+          <View style={styles.rowContainer}>
+            <Text style={styles.itemTitle}>{item.text}</Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    ) : (
       <TouchableHighlight
         style={styles.itemContainer}
         onPress={() => this.props.onPress()}
-        underlayColor="rgba(128, 128, 128, 0.1)"
-      >
+        underlayColor="rgba(128, 128, 128, 0.1)">
         <View style={styles.mainContainer}>
           <View style={styles.rowContainer}>
-            <Image style={styles.itemIcon} source={{ uri: item.icon }} />
+            <Image style={styles.itemIcon} source={item.icon} />
             <View style={styles.itemTxtContainer}>
-              <Text style={styles.itemTitle}>{item.title}</Text>
-              <Text style={styles.itemmText}>{item.text}</Text>
+              <Text style={styles.blueTitle}>{item.title}</Text>
+            </View>
+            <View style={styles.itemTxtContainer}>
+              <Text style={styles.itemTitle}>{item.date}</Text>
             </View>
           </View>
-          <View
-            style={
-              parseFloat(item.procent) < 2
-                ? styles.redProcentContainer
-                : styles.greenProcentContainer
-            }
-          >
-            <Text
-              style={parseFloat(item.procent) < 2 ? styles.redItemProcent : styles.greenItemProcent}
-            >
-              {item.procent + '%'}
-            </Text>
+          <View style={styles.amountContainer}>
+            <Text style={styles.amountItem}>{item.procent + '€'}</Text>
           </View>
         </View>
       </TouchableHighlight>
--- src/screens/HistoriqueJutificatifs/HistoriqueJutificatifsScreen.js
+++ src/screens/HistoriqueJutificatifs/HistoriqueJutificatifsScreen.js
@@ -1,8 +1,10 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable comma-dangle */
 import React from 'react';
-import {View, Text, TouchableHighlight} from 'react-native';
+import {View, Text, TouchableHighlight, FlatList} from 'react-native';
 import {connect} from 'react-redux';
+import CardView from '../../components/CardView/CardView';
+import Camera from '../../../assets/icons/Camera.png';
 
 import styles from './styles';
 
@@ -10,17 +12,100 @@ class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {user: null};
+    this.state = {
+      list: [
+        {id: 0, text: 'octobre le 10/12/2021', isTitle: true},
+
+        {
+          id: 1,
+          icon: Camera,
+          date: 'mm/mm/mm',
+          title: 'recu le',
+          procent: 10,
+          isTitle: false,
+        },
+        {
+          id: 2,
+          icon: Camera,
+          date: 'mm/mm/mm',
+          title: 'recu le',
+          procent: 30.22,
+          isTitle: false},
+          {id: 0, text: 'octobre le 10/12/2021', isTitle: true},
+
+          {
+            id: 1,
+            icon: Camera,
+            date: 'mm/mm/mm',
+            title: 'recu le',
+            procent: 10,
+            isTitle: false,
+          },
+          {
+            id: 2,
+            icon: Camera,
+            date: 'mm/mm/mm',
+            title: 'recu le',
+            procent: 30.22,
+            isTitle: false},
+      ],
+      seed: 1,
+      page: 1,
+      isLoading: false,
+      isRefreshing: false,
+    };
   }
+
+  loadData = () => {
+    const {users, seed, page} = this.state;
+    this.setState({isLoading: true});
+
+    /**   fetch(`https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`)
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          users: page === 1 ? res.results : [...list, ...res.results],
+          isRefreshing: false,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });*/
+  };
+
+  handleRefresh = () => {
+    this.setState(
+      {
+        seed: this.state.seed + 1,
+        isRefreshing: true,
+      },
+      () => {
+        this.loadData();
+      },
+    );
+  };
+
+  handleLoadMore = () => {
+    this.setState(
+      {
+        page: this.state.page + 1,
+      },
+      () => {
+        this.loadData();
+      },
+    );
+  };
+
   componentDidMount() {
-    this.initData();
+    this.loadData();
   }
+
   initData = async () => {};
 
-  renderData = ({item}) => <Text style={{marginBottom: 2}}>{item}</Text>;
+  renderItem = ({item}) => <CardView item={item} />;
 
   render() {
-    const {user} = this.props;
+    const {list, isRefreshing} = this.state;
     return (
       <View style={styles.container}>
         <TouchableHighlight
@@ -34,6 +119,16 @@ class HomeScreen extends React.Component {
             </View>
           </View>
         </TouchableHighlight>
+        <FlatList
+          data={list}
+          renderItem={this.renderItem}
+          keyExtractor={(item) => `${item.id}`}
+          initialNumToRender={5}
+          refreshing={isRefreshing}
+          onRefresh={this.handleRefresh}
+          onEndReached={this.handleLoadMore}
+          onEndThreshold={0}
+        />
       </View>
     );
   }
