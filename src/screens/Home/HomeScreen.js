b38e0b3aa3a001d1710bc6fdecd203d5daf22d94
Bilel MONSER <bilel.monser@econocom.com>
2020-11-26 18:45:31 +0100

    delete unused code

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -17,8 +17,6 @@ class HomeScreen extends React.Component {
     this.initData();
   }
   initData = async () => {
-    console.log('user', this.props.user);
-
     await this.setState({user: this.props.user.user});
   };
 
