f7198d2d14a7a21aac2281894013286e274fd383
Bilel Monser <bilelmonser@MacBook-Air-de-Bilel.local>
2021-02-16 14:27:59 +0100

    change variable name

--- src/services/bankStatement.js
+++ src/services/bankStatement.js
@@ -15,24 +15,27 @@ export default async function getEnterprise(
 ) {
   try {
     let path = `${c.GETENTERPRISE}?limit=${limit}&page=${page}`;
+    
     if (dateDebut != null)
-      path = `${path}&date_debut=${moment(dateDebut).format('DD/MM/YYYY')}`;
-    if (dateFin != null)
-      path = `${path}&date_fin=${moment(dateFin).format('DD/MM/YYYY')}`;
+      path = `${path}&start_date=${moment(dateDebut).format('DD/MM/YYYY')}`;
+    
+      if (dateFin != null)
+      path = `${path}&end_date=${moment(dateFin).format('DD/MM/YYYY')}`;
+    
     if (min != '') path = `${path}&min=${min}`;
-    if (max != '') path = `${path}&max=${max}`;
     if (max != '') path = `${path}&max=${max}`;
     
     if (search_multiple != '')
-      path = `${path}&search_multiple =${search_multiple}`;
+      path = `${path}&search =${search_multiple}`;
     
     if (debit_search != '') 
-      path = `${path}&debit_search=${debit_search}`;
+      path = `${path}&debit=${debit_search}`;
     
     if (numero_compte_search != '')
-      path = `${path}&numero_compte_search=${numero_compte_search}`;
+      path = `${path}&account_number=${numero_compte_search}`;
 
-    console.log('zzzzzzzzzzzzzzzzzzzzzz');
+
+    console.log(path);
     
     let res = await api.get(path, {
       withCredentials: true,
