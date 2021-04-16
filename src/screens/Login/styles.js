2a0c66f48ab02207528b4b4645157bf48a5bbbfd
Makrem Zitoun <makrem.zitoun@x-cite.io>
2021-04-16 13:52:46 +0100

    integration

--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -1,8 +1,6 @@
 /* eslint-disable comma-dangle */
-import {StyleSheet, Dimensions, Platform} from 'react-native';
+import {StyleSheet, Platform} from 'react-native';
 import ScaleHelpers from '../../Theme/scaleHelpers';
-const {width, height} = Dimensions.get('window');
-const SCREEN_WIDTH = width < height ? width : height;
 import {textColor, buttonColor} from '../../Theme/AppStyles';
 import {fontType} from '../../Theme/AppStyles';
 
@@ -10,6 +8,8 @@ import {fontType} from '../../Theme/AppStyles';
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
+    height: ScaleHelpers.CalcHeight(95),
+
   },
   topImageStyle: {
     width: ScaleHelpers.CalcWidth(100),
