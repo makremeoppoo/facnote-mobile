73d5f07443b38380c0073718c1fb0e4a6ee00d58
Makrem Zitoun <makrem.zitoun@x-cite.io>
2021-06-19 16:00:03 +0100

    fix defect

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -78,53 +78,78 @@ class HomeScreen extends React.Component {
   }
   async loadIndicateur(date) {
     this.setState({ loading: true });
+    if (this.state.selectedIndex == 0
+      /*&& Object.values(this.state.turnover).length == 0
+      && Object.values(this.state.fixedCharge).length == 0
+      && Object.values(this.state.notFixedCharge).length == 0
+      && Object.values(this.state.bankBalance).length == 0*/) {
+      console.log(this.state.selectedIndex)
 
-    const indicators = await getIndicator(moment(date).format('YYYY'));
+      let indicators = await getIndicator(moment(date).format('YYYY'));
 
-    const sigIndicators = await getSigIndicator(moment(date).format('YYYY'));
+      const turnover = indicators.data_table_2.find(
+        (item) => item.label == "Chiffre d'affaire",
+      );
+      const fixedCharge = indicators.data_table_2.find(
+        (item) => item.label == 'Charge fixe',
+      );
+      const notFixedCharge = indicators.data_table_2.find(
+        (item) => item.label == 'Charge variable',
+      );
 
-    const turnover = indicators.data_table_2.find(
-      (item) => item.label == "Chiffre d'affaire",
-    );
-    const fixedCharge = indicators.data_table_2.find(
-      (item) => item.label == 'Charge fixe',
-    );
-    const notFixedCharge = indicators.data_table_2.find(
-      (item) => item.label == 'Charge variable',
-    );
+      const bankBalance = indicators.data_table_1.find(
+        (item) => item.label == 'Solde de la banque',
+      );
+      this.setState({
+        turnover,
+        fixedCharge,
+        notFixedCharge,
+        bankBalance,
 
-    const bankBalance = indicators.data_table_1.find(
-      (item) => item.label == 'Solde de la banque',
-    );
+        loading: false,
+      });
+    } else if (this.state.selectedIndex != 0
+      /* && Object.values(this.state.marge).length == 0
+       && Object.values(this.state.excedentBrut).length == 0
+       && Object.values(this.state.chargePersonel).length == 0*/
+    ) {
+      console.log(this.state.selectedIndex)
 
-    const marge = sigIndicators.data.find(
-      (item) => item.label == 'Marge commerciale',
-    );
-    const excedentBrut = sigIndicators.data.find(
-      (item) => item.label == "Excédent brut d'exploitation",
-    );
+      let sigIndicators = await getSigIndicator(moment(date).format('YYYY'));
 
-    const chargePersonel = sigIndicators.data.find(
-      (item) => item.label == 'Charges de personnel',
-    );
 
-    this.setState({
-      turnover,
-      fixedCharge,
-      notFixedCharge,
-      bankBalance,
-      marge,
-      excedentBrut,
-      chargePersonel,
-      loading: false,
-    });
+      const marge = sigIndicators.data.find(
+        (item) => item.label == 'Marge commerciale',
+      );
+      const excedentBrut = sigIndicators.data.find(
+        (item) => item.label == "Excédent brut d'exploitation",
+      );
+
+      const chargePersonel = sigIndicators.data.find(
+        (item) => item.label == 'Charges de personnel',
+      );
+
+      this.setState({
+        marge,
+        excedentBrut,
+        chargePersonel,
+        loading: false,
+      });
+    } else {
+      this.setState({
+        loading: false,
+      });
+    }
+
+
   }
 
-  handleIndexChange = (index) => {
-    this.setState({
+  handleIndexChange = async (index) => {
+    await this.setState({
       ...this.state,
       selectedIndex: index,
     });
+    this.loadIndicateur(this.state.exercise.date_debut)
   };
   getTurnoverValues = () => {
     const { turnover } = this.state;
